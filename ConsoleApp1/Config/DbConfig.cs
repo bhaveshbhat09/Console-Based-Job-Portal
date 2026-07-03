6bcdb7887d2ded8c58aa4c7e1bae89d934fc826a
bhavesh <bhaveshbhat2003@gmail.com>
2026-07-03 18:42:44 +0530

    Connect with SQL Server database

---
+++ ConsoleApp1/Config/DbConfig.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1.Config
+{
+    public static class DbConfig
+    {
+        public static readonly string ConnectionString =
+            "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=JobPortalDb;Integrated Security=True;TrustServerCertificate=True";
+    }
+
+}
